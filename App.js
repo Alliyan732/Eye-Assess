19cd3a5337de2a5eebfc467e593ae593d87f686b
Alliyan <alliyan732@gmail.com>
2023-12-16 17:19:58 +0500

    astigmatism info screen added

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import ColorBlindInfo from './src/screens/VisionAssessments/ColorBlindInfo';
 import ColorBlindTest from './src/screens/VisionAssessments/ColorBlindTest';
 import ContrastSensitivityInfo from './src/screens/VisionAssessments/ContrastSensitivityInfo';
 import ContrastSensitivityTest from './src/screens/VisionAssessments/ContrastSensitivityTest';
+import AstigmatismInfo from './src/screens/VisionAssessments/AstigmatismInfo';
 
 const Stack = createNativeStackNavigator();
 
@@ -27,6 +28,7 @@ function App() {
           <Stack.Screen name="ColorBlindTest" component={ColorBlindTest} options={{ title: "Color Blind Test" }}/>
           <Stack.Screen name="ContrastSensitivityInfo" component={ContrastSensitivityInfo} options={{ title: "Contrast Sensitivity Test" }}/>
           <Stack.Screen name="ContrastSensitivityTest" component={ContrastSensitivityTest} options={{ title: "Contrast Sensitivity Test" }}/>
+          <Stack.Screen name="AstigmatismInfo" component={AstigmatismInfo} options={{ title: "Astigmatism Test" }}/>
         </Stack.Navigator>
       </NavigationContainer>
     )
