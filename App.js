b1e73669c54dc71e843da2d3643154f7c9587cc0
Alliyan <alliyan732@gmail.com>
2023-12-16 17:12:16 +0500

    vision acutiy info screen added

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // Vission Assessments Screens
 import VisionAssessmentHome from './src/screens/VisionAssessments/VisionAssessmentsHome';
-
+import VisionAcuityInfo from './src/screens/VisionAssessments/VisionAcuityInfo';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,6 +16,7 @@ function App() {
         <Stack.Navigator initialRouteName={"VisionAssessmentsHome"}>
           {/* Vission Assessments */}
           <Stack.Screen name="VisionAssessmentsHome" component={VisionAssessmentHome} options={{ title: "Vision Assessments" }}/>
+          <Stack.Screen name="VisionAcuityInfo" component={VisionAcuityInfo} options={{ title: "Vision Acuity Test" }}/>
         </Stack.Navigator>
       </NavigationContainer>
     )
