0e3979760023cc400bfe9a33854c0ef0d38a1a8f
Alliyan <alliyan732@gmail.com>
2023-12-16 16:28:32 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { NavigationContainer, } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+// Vission Assessments Screens
+import VisionAssessmentHome from './src/screens/VisionAssessments/VisionAssessmentsHome';
+
+
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+
+  return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName={"VisionAssessmentsHome"}>
+          {/* Vission Assessments */}
+          <Stack.Screen name="VisionAssessmentsHome" component={VisionAssessmentHome} options={{ title: "Vision Assessments" }}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
+}
+
+export default App;
